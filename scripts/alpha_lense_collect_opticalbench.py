61ad02cbe5403e5ff6b121554e8349f01ada102b
Phil-47 <wangteng.cn@hotmail.com>
2026-09-15 23:38:14 +0800

    Add workstation entrypoint for Alpha Lense corpus collection

---
+++ scripts/alpha_lense_collect_opticalbench.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Bridge-safe entrypoint for Alpha Lense Optical Bench corpus collection."""
+from alpha_lense.data.collect_opticalbench import main
+if __name__ == '__main__':
+    main()
